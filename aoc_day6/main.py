2b3fa264cc96529866264b152b1cc7a018ed87a1
tushar1977 <steve192g@gmail.com>
2024-12-07 12:13:30 +0530

    day 7

--- aoc_day6/main.py
+++ aoc_day6/main.py
@@ -5,60 +5,33 @@ map = []
 for data in raw_data:
     map.append(data.strip())
 
-print(map[0][-1] == "\n")
-print(map)
-
-directions = [[0, -1], [1, 0], [0, 1], [-1, 0]]
-curr_dir = 0
-
 pos = []
-obs_2 = {}
 obs_dict = {}
-
+print(map)
 for y, row in enumerate(map):
     for x, obj in enumerate(row):
+        if obj == "#":
+            obs_dict[f"{x},{y}"] = True
         if obj == "^":
             pos = [x, y]
-        if obj == "#":
-            obs_dict[f"{x};{y}"] = True
-        else:
-            obs_2[f"{x};{y}"] = True
+
+print(map[1][1])
+directions = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+curr_dir = 0
 visited = set()
-cnt = 0
 
+while 0 < pos[0] < len(map[0]) and 0 < pos[1] < len(map):
+    direction = directions[curr_dir]
 
-def detect_loop(x, y, obs_dict, pos, curr_dir):
-    obs_dict[f"{x};{y}"] = True
+    print("current dir", direction)
+    new_pos = [pos[0] + direction[0], pos[1] + direction[1]]
+    print("new pos", new_pos)
 
-    turns_taken = {}
-    turns_taken[f"{pos[0]};{pos[1]};{curr_dir}"] = True
+    if obs_dict.get(f"{new_pos[0]},{new_pos[1]}"):
+        curr_dir += 1
+        curr_dir %= len(directions)
+    else:
+        visited.add(f"{new_pos[0]},{new_pos[1]}")
+        pos = new_pos
 
-    while 0 < pos[0] < len(map[0]) and 0 < pos[1] < len(map):
-        direction = directions[curr_dir]
-        next_pos = [pos[0] + direction[0], pos[1] + direction[1]]
-
-        if obs_dict.get(f"{next_pos[0]};{next_pos[1]}"):
-            curr_dir += 1
-            curr_dir %= len(directions)
-
-            if turns_taken.get(f"{pos[0]};{pos[1]};{curr_dir}"):
-                return True
-            else:
-                turns_taken[f"{pos[0]};{pos[1]};{curr_dir}"] = True
-        else:
-            pos = next_pos
-    return False
-
-
-loops = 0
-for x in range(len(map[0])):
-    for y in range(len(map)):
-        if not obs_dict.get(f"{x};{y}"):
-            if detect_loop(x, y, obs_dict.copy(), pos, curr_dir):
-                print(x, y)
-                loops += 1
-
-
-print(detect_loop(0, 0, obs_dict, pos, curr_dir))
-
-print(loops)
+print(len(visited))
---
+++ aoc_day7/main.py
@@ -0,0 +1,34 @@
+from itertools import product
+
+with open("t2.txt", "r") as f:
+    raw_data = f.read().splitlines()
+
+final_nums = {}
+for line in raw_data:
+    key, values = line.split(": ")
+    final_nums[int(key)] = list(map(int, values.split(" ")))
+
+
+def solve2(k, v):
+    for combo in product("*+|", repeat=len(v) - 1):
+        ans = v[0]
+        for i in range(1, len(v)):
+            if combo[i - 1] == "+":
+                ans += v[i]
+            elif combo[i - 1] == "|":
+                ans = int(str(ans) + str(v[i]))
+            else:
+                ans *= v[i]
+
+        if ans == k:
+            print(f"found{k}\n, {v}\n, {combo}")
+            return True
+    return False
+
+
+ans2 = 0
+for k, v in final_nums.items():
+    if solve2(k, v):
+        ans2 += k
+
+print("Sum of keys that satisfy the condition:", ans2)
